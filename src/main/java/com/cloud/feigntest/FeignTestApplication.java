eb30546584692ce8128d13051107f4417ec641b7
JLS <758065565@qq.com>
2022-06-14 21:58:58 +0800

    测试

--- src/main/java/com/cloud/feigntest/FeignTestApplication.java
+++ src/main/java/com/cloud/feigntest/FeignTestApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class FeignTestApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(FeignTestApplication.class, args);
     }
 
